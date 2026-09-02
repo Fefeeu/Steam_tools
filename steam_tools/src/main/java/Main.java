1a07449191285fced3a55ac4a3e4cae6bb7d98f0
AndreDB19 <andrediasbalbino19@gmail.com>
2026-09-02 20:15:30 -0300

    temporizador

--- steam_tools/src/main/java/Main.java
+++ steam_tools/src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 import org.steam.LoopThread;
 import org.steam.Produto;
+import org.steam.api.ProductAPI;
 import org.steam.api.UserAPI;
 
 import java.util.Arrays;
@@ -8,7 +9,7 @@ import java.util.Comparator;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Produto[] listajogos = UserAPI.getJogos("76561198241059866");
 
@@ -22,9 +23,31 @@ public class Main {
         t1.setI(1);
         t1.setListaJogos(listajogos);
 
+        //sem thread
+        long tempoInicial = System.currentTimeMillis();
+
+        for(int i = 0; i < listajogos.length; i ++)
+        {
+            listajogos[i] = ProductAPI.getJogo(listajogos[i].getAppid(), listajogos[i].getDiasSemJogar());
+        }
+        long tempoFinal = System.currentTimeMillis();
+
+        float tempoTotal = (tempoFinal - tempoInicial)/1000;
+
+        //Com thread
+        tempoInicial = System.currentTimeMillis();
+
         t0.start();
         t1.start();
 
+        t0.join();
+        t1.join();
 
+        tempoFinal = System.currentTimeMillis();
+
+        float tempoTotalThread = (tempoFinal - tempoInicial)/1000;
+
+        System.out.println("Tempo total gasto com uma thread: " + tempoTotal + " s");
+        System.out.println("Tempo total gasto em paralelo: " + tempoTotalThread + " s");
     }
 }
