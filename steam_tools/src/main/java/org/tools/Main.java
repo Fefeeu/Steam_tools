587f82c491a24ec5e3c2848d9bc04666b71b347b
Fefeeu <felipefcgp.se@gmail.com>
2026-09-09 17:35:26 -0300

    limpando main

--- steam_tools/src/main/java/org/tools/Main.java
+++ steam_tools/src/main/java/org/tools/Main.java
@@ -5,23 +5,6 @@ import org.tools.steam.Usuario;
 
 public class Main {
     static void main() {
-        Usuario eu = new Usuario("76561198299393168");
-
-        Thread threadObservador = new Thread(() -> {
-            while (true) {
-                eu.atualizarListaDeDesejos();
-                Notificacao.wishList(eu);
-                eu.atualizaBeckupWishList();
-
-                try {
-                    Thread.sleep(5); // 5 segundos entre checagens, ajuste como quiser
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        });
-
-        threadObservador.start();
+        
     }
 }
