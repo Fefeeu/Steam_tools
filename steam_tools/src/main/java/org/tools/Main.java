f059095c5e3c7dac13279d183d86496d9c2b8625
Maria Rita <mariaritaraposo@outlook.com>
2026-09-09 17:37:10 -0300

    limpando a main

--- steam_tools/src/main/java/org/tools/Main.java
+++ steam_tools/src/main/java/org/tools/Main.java
@@ -1,11 +1,7 @@
 package org.tools;
 
-import org.tools.steam.api.GameAPI;
-import org.tools.steam.api.UserAPI;
-
 public class Main {
-    static void main() {
-        System.out.println(GameAPI.getGame("250900").toString()
-        );
+
+    public static void main(String[] args) {
     }
 }
