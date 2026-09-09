f37ee5abed0e68794e6ecabc4a5fc05117d86281
Fefeeu <felipefcgp.se@gmail.com>
2026-09-09 19:04:15 -0300

    primeira versão das threads em paralelo e sequencial

--- steam_tools/src/main/java/org/Main.java
+++ steam_tools/src/main/java/org/Main.java
@@ -1,9 +1,7 @@
 package org;
 
-import org.output.Notificacao;
 import org.steam.LoopThread;
 import org.steam.Produto;
-import org.steam.Usuario;
 import org.steam.api.ProductAPI;
 import org.steam.api.UserAPI;
 
@@ -13,23 +11,44 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Usuario eu = new Usuario("76561198299393168");
 
-        Thread threadObservador = new Thread(() -> {
-            while (true) {
-                eu.atualizarListaDeDesejos();
-                Notificacao.wishList(eu);
-                eu.atualizaBeckupWishList();
+        Produto[] listajogos = UserAPI.getJogos("76561198241059866");
 
-                try {
-                    Thread.sleep(5); // 5 segundos entre checagens, ajuste como quiser
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        });
+        Arrays.sort(listajogos, Comparator.comparing(Produto::getDiasSemJogar).reversed());
 
-        threadObservador.start();
+        LoopThread t0 = new LoopThread();
+        t0.setI(0);
+        t0.setListaJogos(listajogos);
+
+        LoopThread t1 = new LoopThread();
+        t1.setI(1);
+        t1.setListaJogos(listajogos);
+
+        //sem thread
+        long tempoInicial = System.currentTimeMillis();
+
+        for(int i = 0; i < listajogos.length; i ++)
+        {
+            listajogos[i] = ProductAPI.getJogo(listajogos[i].getAppid(), listajogos[i].getDiasSemJogar());
+        }
+        long tempoFinal = System.currentTimeMillis();
+
+        float tempoTotal = (tempoFinal - tempoInicial)/1000;
+
+        //Com thread
+        tempoInicial = System.currentTimeMillis();
+
+        t0.start();
+        t1.start();
+
+        t0.join();
+        t1.join();
+
+        tempoFinal = System.currentTimeMillis();
+
+        float tempoTotalThread = (tempoFinal - tempoInicial)/1000;
+
+        System.out.println("Tempo total gasto com uma thread: " + tempoTotal + " s");
+        System.out.println("Tempo total gasto em paralelo: " + tempoTotalThread + " s");
     }
 }
